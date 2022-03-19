3b8d2c689a7894aedd2e94a269afdad3c2b859a2
ratkorajkovic <45713322+ratkorajkovic@users.noreply.github.com>
2022-03-19 12:26:36 +0100

    Update Main.java
    Bolje je ovako.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Neki sadrzaj");
         A a = new A();
-        a.metoda();
+        a.funkcija();
     }
 }
